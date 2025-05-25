fced42e108fd8548e5fc2797e78e61e4a12a34d6
belema <hectorarbelaezpaz@gmail.com>
2025-05-25 09:54:13 -0500

    tendencies done; pending: growth rate and participation

--- eda.py
+++ eda.py
@@ -32,9 +32,11 @@ def translate_log_transform(grouped_df:pd.DataFrame) -> dict:
     
 def group_dataframe():
     df = initialize()
-    grouped_df = df.groupby(['Year', 'Partner', 'HSCode'], observed=False)['RealValue']\
+    grouped_df = df.groupby(['Year', 'Partner', 'HSCode', 'Flow'], observed=False)['RealValue']\
         .sum().reset_index()
     return grouped_df
+
+
 
 # --- Distributions --- 
 def unidistributions_simple():
@@ -138,6 +140,179 @@ def bivariate_distribution():
         plt.savefig(f'images/bidistributions/bins20/bidisb20_{partner}.png')
         plt.show()
         plt.close()
+
+
+
+# --- Tendencias ---
+def simple_total_tendencies():
+    df = group_dataframe()
+    df = log_transform(df)
+
+    switz = df.query('Partner == "Switzerland"')
+    pre_switz = switz[switz['Year'] <= 2011]
+    post_switz = switz[switz['Year'] >= 2012]
+
+    partners = df['Partner'].unique()
+
+    for partner in partners:
+
+        fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(12, 8), layout='constrained')
+        fig.suptitle('Tendencies')
+
+        pre_df = df[(df['Partner'] == partner) & (df['Year'] <= 2011)]
+        post_df = df[(df['Partner'] == partner) & (df['Year'] >= 2012)]
+
+        sns.lineplot(x=pre_df['Year'], y=pre_df['LogRealValue'], ax=ax[0][0])
+        sns.lineplot(x=pre_switz['Year'], y=pre_switz['LogRealValue'], ax=ax[0][1])
+        sns.lineplot(x=post_df['Year'], y=post_df['LogRealValue'], ax=ax[1][0])
+        sns.lineplot(x=post_switz['Year'], y=post_switz['LogRealValue'], ax=ax[1][1])
+
+        ax[0][0].set_title('Pre TLC ' + partner)
+        ax[0][1].set_title('Pre TLC Switzerland')
+        ax[1][0].set_title('Post TLC ' + partner)
+        ax[1][1].set_title('Post TLC Switzerland')
+        
+        plt.savefig(f'images/tendencies/simple/total/tensimtot_{partner}.png')
+        plt.show()
+        plt.close()
+
+def simple_flow_tendencies():
+    df = group_dataframe()
+    df = log_transform(df)
+
+    switz = df.query('Partner == "Switzerland"')
+    pre_switz = switz[switz['Year'] <= 2011]
+    post_switz = switz[switz['Year'] >= 2012]
+
+    partners = df['Partner'].unique()
+
+    for partner in partners:
+
+        fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(12, 8), layout='constrained')
+        fig.suptitle('Tendencies')
+
+        pre_df = df[(df['Partner'] == partner) & (df['Year'] <= 2011)]
+        post_df = df[(df['Partner'] == partner) & (df['Year'] >= 2012)]
+
+        sns.lineplot(x=pre_df['Year'], y=pre_df['LogRealValue'], hue=pre_df['Flow'], ax=ax[0][0])
+        sns.lineplot(x=pre_switz['Year'], y=pre_switz['LogRealValue'], hue=pre_switz['Flow'], ax=ax[0][1])
+        sns.lineplot(x=post_df['Year'], y=post_df['LogRealValue'], hue=post_df['Flow'], ax=ax[1][0])
+        sns.lineplot(x=post_switz['Year'], y=post_switz['LogRealValue'], hue=post_switz['Flow'], ax=ax[1][1])
+
+        ax[0][0].set_title('Pre TLC ' + partner)
+        ax[0][1].set_title('Pre TLC Switzerland')
+        ax[1][0].set_title('Post TLC ' + partner)
+        ax[1][1].set_title('Post TLC Switzerland')
+        
+        plt.savefig(f'images/tendencies/simple/flow/tensimflo_{partner}.png')
+        plt.show()
+        plt.close()
+
+def complex_total_tendencies():
+    df = group_dataframe()
+    df = log_transform(df)
+
+    switz = df.query('Partner == "Switzerland"')
+    pre_switz = switz[switz['Year'] <= 2011]
+    post_switz = switz[switz['Year'] >= 2012]
+
+    partners = df['Partner'].unique()
+
+    for partner in partners:
+
+        fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(20, 12), layout='constrained')
+        fig.suptitle('Tendencies')
+
+        pre_df = df[(df['Partner'] == partner) & (df['Year'] <= 2011)]
+        post_df = df[(df['Partner'] == partner) & (df['Year'] >= 2012)]
+
+        sns.barplot(x=pre_df['Year'], y=pre_df['LogRealValue'], hue=pre_df['HSCode'], errorbar=None, ax=ax[0][0])
+        sns.barplot(x=pre_switz['Year'], y=pre_switz['LogRealValue'], hue=pre_switz['HSCode'], errorbar=None, ax=ax[0][1])
+        sns.barplot(x=post_df['Year'], y=post_df['LogRealValue'], hue=post_df['HSCode'], errorbar=None, ax=ax[1][0])
+        sns.barplot(x=post_switz['Year'], y=post_switz['LogRealValue'], hue=post_switz['HSCode'], errorbar=None, ax=ax[1][1])
+
+        ax[0][0].set_title('Pre TLC ' + partner)
+        ax[0][1].set_title('Pre TLC Switzerland')
+        ax[1][0].set_title('Post TLC ' + partner)
+        ax[1][1].set_title('Post TLC Switzerland')
+        
+        plt.savefig(f'images/tendencies/complex/total/tencomtot_{partner}.png')
+        plt.show()
+        plt.close()
+
+def complex_flow_tendencies():
+    df = group_dataframe()
+    df = log_transform(df)
+
+    switz = df.query('Partner == "Switzerland"')
+    pre_mswitz = switz[(switz['Flow'] == 'M') & (switz['Year'] <= 2011)]
+    pre_xswitz = switz[(switz['Flow'] == 'X') & (switz['Year'] <= 2011)]
+    post_mswitz = switz[(switz['Flow'] == 'M') & (switz['Year'] >= 2012)]
+    post_xswitz = switz[(switz['Flow'] == 'X') & (switz['Year'] >= 2012)]
+
+    partners = df['Partner'].unique()
+
+    for partner in partners:
+
+        # Instanties figure and subfigures with axes
+        fig = plt.figure(figsize=(24, 16), layout='constrained')
+        fig.suptitle('Tendencies')
+
+        fig_up, fig_down = fig.subfigures(nrows=2, ncols=1)
+        axs_up = fig_up.subplots(nrows=2, ncols=2)
+        axs_down = fig_down.subplots(nrows=2, ncols=2)
+
+        fig_up.suptitle('Imports')
+        fig_down.suptitle('Exports')
+
+        # filters dataframes
+        pre_mdf = df[(df['Partner'] == partner) & (df['Flow'] == 'M') & (df['Year'] <= 2011)]
+        pre_xdf = df[(df['Partner'] == partner) & (df['Flow'] == 'X') & (df['Year'] <= 2011)]
+        post_mdf = df[(df['Partner'] == partner) & (df['Flow'] == 'M') & (df['Year'] >= 2012)]
+        post_xdf = df[(df['Partner'] == partner) & (df['Flow'] == 'X') & (df['Year'] >= 2012)]
+
+        # Create artists for each axe
+        ## Imports
+        au00 = axs_up[0][0]
+        gu1 = sns.barplot(x=pre_mdf['Year'], y=pre_mdf['LogRealValue'], hue=pre_mdf['HSCode'], errorbar=None, ax=au00)
+
+        au01 = axs_up[0][1]
+        gu2 = sns.barplot(x=pre_mswitz['Year'], y=pre_mswitz['LogRealValue'], hue=pre_mswitz['HSCode'], errorbar=None, ax=au01)
+
+        au10 = axs_up[1][0]
+        gu3 = sns.barplot(x=post_mdf['Year'], y=post_mdf['LogRealValue'], hue=post_mdf['HSCode'], errorbar=None, ax=au10)
+
+        au11 = axs_up[1][1]
+        gu4 = sns.barplot(x=post_mswitz['Year'], y=post_mswitz['LogRealValue'], hue=post_mswitz['HSCode'], errorbar=None, ax=au11)
+
+        ## Exports
+        ad00 = axs_down[0][0]
+        gd1 = sns.barplot(x=pre_xdf['Year'], y=pre_xdf['LogRealValue'], hue=pre_xdf['HSCode'], errorbar=None, ax=ad00)
+
+        ad01 = axs_down[0][1]
+        gd2 = sns.barplot(x=pre_xswitz['Year'], y=pre_xswitz['LogRealValue'], hue=pre_xswitz['HSCode'], errorbar=None, ax=ad01)
+
+        ad10 = axs_down[1][0] 
+        gd3 = sns.barplot(x=post_xdf['Year'], y=post_xdf['LogRealValue'], hue=post_xdf['HSCode'], errorbar=None, ax=ad10)
+
+        ad11 = axs_down[1][1]
+        gd4 = sns.barplot(x=post_xswitz['Year'], y=post_xswitz['LogRealValue'], hue=post_xswitz['HSCode'], errorbar=None, ax=ad11)
+
+        # Axes titles
+        au00.set_title('Pre TLC ' + partner)
+        au01.set_title('Pre TLC Switzerland')
+        au10.set_title('Post TLC ' + partner)
+        au11.set_title('Post TLC Switzerland')
+
+        ad00.set_title('Pre TLC ' + partner)
+        ad01.set_title('Pre TLC Switzerland')
+        ad10.set_title('Post TLC ' + partner)
+        ad11.set_title('Post TLC Switzerland')
+        
+        plt.savefig(f'images/tendencies/complex/flow/tencomflo_{partner}.png')
+        plt.show()
+        plt.close()
+
 
 if __name__ == '__main__':
     print('Executing eda.py')
